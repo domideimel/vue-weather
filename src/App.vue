7d7fd7a30355c6fb85ecb5f6cf7d4c37b06787e4
dominikdeimel <info@dominikdeimel.com>
2024-03-20 21:26:32 +0100

    chore(geolocation): added geolocation info and show error

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
 import Header from '@/components/Header.vue'
-import { useGeolocation } from '@vueuse/core'
+import { computedAsync, useGeolocation } from '@vueuse/core'
 import { useQuery, useQueryClient } from '@tanstack/vue-query'
 import { fetchWeather } from '@/lib/queries/fetchWeather.queries.ts'
 import { computed, watch } from 'vue'
+import { AlertCircle } from 'lucide-vue-next'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 
 const { coords } = useGeolocation()
 const queryClient = useQueryClient()
+
+const hasGeoPermission = computedAsync(
+  async () => {
+    return (await navigator.permissions.query({ name: 'geolocation' })).state
+  },
+  'denied'
+)
 
 const lat = computed(() => {
   return Number.isFinite(coords.value.latitude) ? coords.value.latitude : null
@@ -21,19 +30,21 @@ watch([lat, lon], ([lat, lon]) => {
 })
 
 const { data } = useQuery(fetchWeather({ lat: lat.value ?? 0, lon: lon.value ?? 0 }))
-
-console.log(data.value)
 </script>
 
 <template>
   <div class="min-h-screen">
     <Header/>
-    <div class="container flex flex-col">
+    <div class="container flex flex-col mt-5">
       <main class="flex-1">
-        <pre>
-          lat: {{ coords.latitude }}
-          lon: {{ coords.longitude }}
-        </pre>
+        <Alert v-if="hasGeoPermission === 'denied' && !data" variant="destructive">
+          <AlertCircle class="w-4 h-4"/>
+          <AlertTitle>Error</AlertTitle>
+          <AlertDescription>
+            Du hast die Standortfreigabe abgelehnt. Bitte erlaube den Zugriff auf deinen Standort, um das Wetter zu
+            sehen oder nutze die Suche.
+          </AlertDescription>
+        </Alert>
       </main>
     </div>
   </div>
