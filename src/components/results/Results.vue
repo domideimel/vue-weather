0ef552b11a3e8a796327d65523506da2e9d1cf70
dominikdeimel <info@dominikdeimel.com>
2024-03-29 20:57:09 +0100

    chore(components): added some results

--- src/components/results/Results.vue
+++ src/components/results/Results.vue
@@ -4,6 +4,10 @@ import { fetchWeather } from '@/lib/queries/fetchWeather.queries.ts'
 import { useCoordinatesStore } from '@/lib/store/coordinates.store.ts'
 import { storeToRefs } from 'pinia'
 import { computed } from 'vue'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Skeleton } from '@/components/ui/skeleton'
+import { HomeModernIcon } from '@heroicons/vue/20/solid'
+import { Separator } from '@/components/ui/separator'
 
 const store = useCoordinatesStore()
 const { lat, lon } = storeToRefs(store)
@@ -18,18 +22,53 @@ const { data } = useQuery({
 </script>
 
 <template>
-  <h3>Results</h3>
-  <div>
-    <p>{{ store.lat }}</p>
-    <p>{{ store.lon }}</p>
-  </div>
 
-  <div v-if="data">
-    <p>{{ data.name }}</p>
-    <p>{{ data.main.temp }}</p>
-    <p>{{ data.weather[0].description }}</p>
-  </div>
-  <div v-else>
-    <p>Loading...</p>
-  </div>
+  <Card>
+    <CardHeader>
+      <CardTitle>
+        <template v-if="!isEnabled">
+          <Skeleton class="w-1/2 h-4"/>
+        </template>
+        <template v-else>
+          <div class="flex gap-1 items-end">
+            <HomeModernIcon class="size-5"/>
+            <span>{{ data?.name }}, {{ data?.sys.country }}</span>
+          </div>
+        </template>
+      </CardTitle>
+      <CardDescription>
+        <template v-if="!isEnabled">
+          <Skeleton class="w-1/4 h-3"/>
+        </template>
+        <template v-else>
+          {{ data?.weather[0].description }}
+        </template>
+      </CardDescription>
+    </CardHeader>
+    <CardContent>
+      <div class="space-y-3">
+        <template v-if="!isEnabled">
+          <Skeleton class="w-1/2 h-6"/>
+          <Skeleton class="w-1/2 h-6"/>
+          <Skeleton class="w-1/2 h-6"/>
+          <Skeleton class="w-1/2 h-6"/>
+        </template>
+        <template v-else>
+          <div v-if="data?.main?.temp">
+            Temperatur: {{ data.main.temp }}°C
+          </div>
+          <div v-if="data?.dt">
+            Zeit der Datenermittlung: {{ new Date(data.dt * 1000).toLocaleTimeString() }} Uhr
+          </div>
+          <Separator/>
+          <div v-if="data?.sys?.sunrise">
+            Sonnenaufgang: {{ new Date(data.sys.sunrise * 1000).toLocaleTimeString() }}
+          </div>
+          <div v-if="data?.sys?.sunset">
+            Sonnenuntergang: {{ new Date(data.sys.sunset * 1000).toLocaleTimeString() }}
+          </div>
+        </template>
+      </div>
+    </CardContent>
+  </Card>
 </template>
